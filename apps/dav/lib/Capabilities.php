e76b07721834a7ba07d7fdb8b1a6e9c3b20817d8
Marcel Müller <marcel-mueller@gmx.de>
2024-12-01 22:22:38 +0100

    chore(absence): Add capability for absence api
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

--- apps/dav/lib/Capabilities.php
+++ apps/dav/lib/Capabilities.php
@@ -26,6 +26,7 @@ namespace OCA\DAV;
 
 use OCP\Capabilities\ICapability;
 use OCP\IConfig;
+use OCP\User\IAvailabilityCoordinator;
 
 class Capabilities implements ICapability {
 	private IConfig $config;
@@ -35,7 +36,7 @@ class Capabilities implements ICapability {
 	}
 
 	/**
-	 * @return array{dav: array{chunking: string, bulkupload?: string}}
+	 * @return array{dav: array{chunking: string, bulkupload?: string, absence-supported?: bool}}
 	 */
 	public function getCapabilities() {
 		$capabilities = [
@@ -45,6 +46,9 @@ class Capabilities implements ICapability {
 		];
 		if ($this->config->getSystemValueBool('bulkupload.enabled', true)) {
 			$capabilities['dav']['bulkupload'] = '1.0';
+		}
+		if ($this->coordinator->isEnabled()) {
+			$capabilities['dav']['absence-supported'] = true;
 		}
 		return $capabilities;
 	}
